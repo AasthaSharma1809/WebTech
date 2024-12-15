3700bd6a9140455f98db8a466571f2422a0f0cea
AasthaSharma1809 <aash22cse@cmrit.ac.in>
2024-12-16 00:16:04 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <?php
+session_start();
 header("Content-Type: application/json");
 
 // Load existing users from JSON file
 $usersFile = 'users.json';
 $users = file_exists($usersFile) ? json_decode(file_get_contents($usersFile), true) : [];
-
 
 $email = $_POST['email'];
 $password = $_POST['password'];
@@ -12,6 +12,13 @@ $password = $_POST['password'];
 // Check credentials
 foreach ($users as $user) {
     if ($user['email'] === $email && password_verify($password, $user['password'])) {
+        // Store user data in session
+        $_SESSION['user'] = [
+            'name' => $user['name'],
+            'email' => $user['email'],
+            'phone' => $user['phone'],
+            'gender' => $user['gender']
+        ];
         echo json_encode(["success" => true, "message" => "Logged in successfully!"]);
         exit;
     }
